f5ebb1f9b3ac5d875ba509627092ce3e2bd69d6d
Xander <poulosar@gmail.com>
2021-06-15 23:22:53 -0400

    Created function for Total_months to allow it to be called on later if needed

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,19 +4,22 @@ from typing import Counter
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-#Uses budget_data.csv to get total number of months in csv, this should work on any number of entries
-with open(csvpath) as csvfile:
+#Function that uses budget_data.csv to get total number of months in csv, this should work on any number of entries
+def total_months():
+    with open(csvpath) as csvfile:
     #set delimiter as ,
-    csvreader = csv.reader(csvfile, delimiter=',')
+        csvreader = csv.reader(csvfile, delimiter=',')
 
-    #skip headers
-    next(csvreader, None)
+        #skip headers
+        next(csvreader, None)
 
-    num_rows = 0
-    #Counts each row until none left and then prints output of counter as total months
-    for row in csvreader:
-        num_rows += 1
-    print("Total Months: " +  str(num_rows))
+        num_rows = 0
+        #Counts each row until none left and then prints output of counter as total months
+        for row in csvreader:
+            num_rows += 1
+        return(str(num_rows))
+#Prints the above function with the string "Total Months"
+print("Total Months: " + str(total_months()))
 
 #Uses budget_data.csv to add all month totals and print it out
 Total = []
@@ -32,3 +35,15 @@ with open(csvpath) as csvfile:
     print("Total: " + "$" + str(sum(Total)))
 
 #Uses budget_data.csv to find an average of changes over time
+average = []
+with open(csvpath) as csvfile:
+    #set delimiter as ,
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    #skip headers
+    next(csvreader, None)
+    #Adds each row, column 2 to the list total and then prints the sum of each row
+    for row in csvreader:
+        Total.append(int(row[1]))
+    
+    print("Total: " + "$" + str(sum(Total)))
