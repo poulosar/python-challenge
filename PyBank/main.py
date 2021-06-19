a35f7e862f94906f92254336b1ef0cec4f5e4394
Xander <poulosar@gmail.com>
2021-06-19 00:15:15 -0400

    Final edits to comments, formatting and bug fixing. Analysis to come

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,52 +1,39 @@
 import os
 import csv
-from typing import Counter
+#Creates reference for file paths
+input = os.path.join('Resources', 'budget_data.csv')
+output_txt = os.path.join('Analysis', 'data_analysis.txt')
 
-csvpath = os.path.join('Resources', 'budget_data.csv')
-
+#creates lists and variables
 months = []
 total_change = []
+change_over_time = []
+count = 0
+last_revenue = 0
+current_revenue = 0
+difference_list = []
+full_list = []
 
-with open(csvpath) as csvfile:
+with open(input) as csvfile:
     # set delimiter as ,
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # skip headers
-    next(csvreader, None)
-
+    #store header rows into a Headers list
+    Headers = next(csvreader)
+    
     num_rows = 0
     # Counts each row until none left and then prints output of counter as total months
     for row in csvreader:
         num_rows += 1
+        total_change.append(int(row[1]))
     months.append(num_rows)
 
-# Uses budget_data.csv to add all month totals and print it out
-with open(csvpath) as csvfile:
+with open(input) as csvfile:
     # set delimiter as ,
     csvreader = csv.reader(csvfile, delimiter=',')
-
     # skip headers
     next(csvreader, None)
-    # Appends net_change with "Profit/Loses" column data
-    for row in csvreader:
-        total_change.append(int(row[1]))
-    # Returns a value of the sum of all values in "Profit/Loses"
 
-# Uses budget_data.csv to get average of changes over time
-change_over_time = []
-with open(csvpath) as csvfile:
-    # set delimiter as ,
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-    # skip headers
-    next(csvreader, None)
-    # variables for counting, last total profit/loss, and current profit/loss
-    count = 0
-    last_revenue = 0
-    current_revenue = 0
-    # List to keep track of each difference to add and average later
-    difference_list = []
-    full_list = []
     # Counter to find row we're on for adding and average
     if count != int(int(num_rows) - 1):
         for row in csvreader:
@@ -56,18 +43,28 @@ with open(csvpath) as csvfile:
             current_revenue = change_over_time[count]
             difference_list.append(current_revenue-last_revenue)
             count = count + 1
-    #sets max_value equal to max profit from the difference list
-    #gets max value and converts to index, this will be used to pull date of this profit
+    # Sets max_value equal to max profit and min_value to min profit
     max_value = max(difference_list)
     min_value = min(difference_list)
+    # Gets max value and converts to index, this will be used to pull date of this profit
     max_value_index = difference_list.index(max_value)
     min_value_index = difference_list.index(min_value)
     # This will remove the first entry, this will be 0 and skews results without it, needs to happen after indexing too
     difference_list.pop(0)
-    # Prints the average change from function defined above
-    print(f'Total Months: {int(num_rows)}')
-    print(f'Total: ${int(sum(total_change))}')
-    print(f'Average Change: ${round(sum(difference_list)/len(difference_list), 2)}')
-    print(f'Greatest increase in profits: {full_list[int(max_value_index)]}  (${max_value})')
-    print(f'Greatest increase in profits: {full_list[int(min_value_index)]}  (${min_value})')
-  
+    # Prints in console to confirm it runs
+    print('\nFinancial Analysis\n')
+    print('--------------------------------\n')
+    print(f'Total Months: {int(num_rows)}\n')
+    print(f'Total: ${int(sum(total_change))}\n')
+    print(f'Average Change: ${round(sum(difference_list)/len(difference_list), 2)}\n')
+    print(f'Greatest increase in profits: {full_list[int(max_value_index)]}  (${max_value})\n')
+    print(f'Greatest decrease in profits: {full_list[int(min_value_index)]}  (${min_value})\n')
+#prints to text file reference
+with open(output_txt, 'w') as output:
+    output.write('\nFinancial Analysis\n')
+    output.write('\n--------------------------------\n')
+    output.write(f'\nTotal Months: {int(num_rows)}\n')
+    output.write(f'\nTotal: ${int(sum(total_change))}\n')
+    output.write(f'\nAverage Change: ${round(sum(difference_list)/len(difference_list), 2)}\n')
+    output.write(f'\nGreatest increase in profits: {full_list[int(max_value_index)]}  (${max_value})\n')
+    output.write(f'\nGreatest decrease in profits: {full_list[int(min_value_index)]}  (${min_value})\n')
--- PyPol/main.py
+++ PyPol/main.py
@@ -10,51 +10,48 @@ candidate = []
 unique_candidate = []
 percent = []
 #Print Statements
-print("\n")
-print("Election Results")
-print("\n---------------------------\n")
 
-#Opens excel file
+# Opens csv data to import
 with open(input_csvpath) as csvfile:
-
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
-    #store header rows into a Headers list
-    Headers = next(csvreader)
-    #set to 0 for counting
-    total_votes = 0
-    # creates unique lists
-    for row in csvreader:
-        total_votes += 1
+    # Opens text file for export
+    with open(output_txt, 'w') as output:
+        # Print statements will appear to duplicate, 
+        # output.read will send values to .txt, print to terminal
+        output.write("\n")
+        print("\n")
+        output.write("\nElection Results\n")
+        print("Election Results")
+        output.write("\n---------------------------\n")
+        print("\n---------------------------\n")
+        # CSV reader specifies delimiter and variable that holds contents
+        csvreader = csv.reader(csvfile, delimiter=',')
+        #store header rows into a Headers list
+        Headers = next(csvreader)
+        #set to 0 for counting
+        total_votes = 0
+        # creates unique lists
+        for row in csvreader:
+            total_votes += 1
+            
+            #Determines candidate uniqueness and adds name to list
+            if row[2] not in unique_candidate:
+                unique_candidate.append(row[2])
+                vote_id.append(1)
+            else:
+                candidate_index = unique_candidate.index(row[2])
+                vote_id[candidate_index] += 1
+        output.write(f'\nTotal Votes: {total_votes}\n')
+        print(f'Total Votes: {total_votes}')
+        output.write("\n---------------------------\n")
+        print("---------------------------")
         
-        #Determines candidate uniqueness and adds name to list
-        if row[2] not in unique_candidate:
-            unique_candidate.append(row[2])
-            vote_id.append(1)
-        else:
-            candidate_index = unique_candidate.index(row[2])
-            vote_id[candidate_index] += 1
-    print(f'Total Votes: {total_votes}')
-    print("---------------------------")
-    #prints each candidate name, % of votes, and total votes
-    for row in range(len(vote_id)):
-        percent.append(vote_id[row] / total_votes)
-        percentage = round(100*(percent[row]))
-        print(f'{unique_candidate[row]}: {percentage:.3f}% ({vote_id[row]})')
-    print(f'---------------------------------------')
-    print(f'Winner: {unique_candidate[0]}!!!')
-
-#Prints output of the above code
-with open(output_txt, 'w') as output:
-    output.write(f"Election Results\n"
-                   f"----------------------------\n"
-                   f"Total Votes: {total_votes}\n"
-                   f"----------------------------\n"
-                   )
-    for i in range(len(unique_candidate)):
-        output.write(f"{unique_candidate[i]}: {percentage:.3f} ({vote_id[i]})\n")
-
-    output.write(f"----------------------------\n"
-                   f"Winner: {unique_candidate[0]}\n"
-                   f"----------------------------\n"
-                  )    
+        #prints each candidate name, % of votes, and total votes
+        for row in range(len(vote_id)):
+            percent.append(vote_id[row] / total_votes)
+            percentage = round(100*(percent[row]))
+            output.write(f'\n{unique_candidate[row]}: {percentage:.3f}% ({vote_id[row]})\n')
+            print(f'{unique_candidate[row]}: {percentage:.3f}% ({vote_id[row]})\n')
+        output.write(f'\n---------------------------------------\n')
+        print(f'---------------------------------------')
+        output.write(f'\nWinner: {unique_candidate[0]}!!!\n')
+        print(f'Winner: {unique_candidate[0]}!!!')
